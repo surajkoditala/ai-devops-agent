ef1dea1b9181db5cc5317b46bd258e49f63957eb
Suraj Koditala <suraj.koditala@myridius.com>
2026-04-24 16:41:06 -0500

    gh iam role and other updates

--- terraform-review-agent/terraform/iam.tf
+++ terraform-review-agent/terraform/iam.tf
@@ -56,3 +56,41 @@ resource "aws_iam_role_policy_attachment" "basic_logs" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+module "iam_oidc_provider" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-oidc-provider"
+
+  url = "https://token.actions.githubusercontent.com"
+
+}
+
+resource "aws_iam_role" "oidc_role" {
+  name = "terraform-ai-oidc-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : module.iam_oidc_provider.arn #"arn:aws:iam::<ACCOUNT_ID>:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          "StringLike" = {
+            "token.actions.githubusercontent.com:sub" = "repo:surajkoditala/ai-devops-agent:*"
+          }
+        }
+      }
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy_attachment" "oidc_admin" {
+  role       = aws_iam_role.oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
